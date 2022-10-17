89b36d860258816c8266c21bd59444f5474175b9
Admin <aborisov96@yandex.ru>
2022-10-17 20:30:59 +0200

    clean code

--- HomeWork_7_7/Program.cs
+++ HomeWork_7_7/Program.cs
@@ -1,19 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography;
 
 namespace HomeWork_7_7
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Army army = new Army();
+            var army = new Army();
             army.TransferSoldier();
             army.Start();
         }
     }
+
     internal class Army
     {
         private List<Soldier> _squadSoldiers1;
@@ -22,37 +22,37 @@ namespace HomeWork_7_7
         public Army()
         {
             _squadSoldiers1 = new List<Soldier>();
-            _squadSoldiers1.Add(new Soldier(name: "Brandon"));
-            _squadSoldiers1.Add(new Soldier(name: "Kevin"));
-            _squadSoldiers1.Add(new Soldier(name: "Kenneth"));
-            _squadSoldiers1.Add(new Soldier(name: "Bruce"));
-            _squadSoldiers1.Add(new Soldier(name: "Billy"));
-            _squadSoldiers1.Add(new Soldier(name: "Joseph"));
-            _squadSoldiers1.Add(new Soldier(name: "Paul"));
-            _squadSoldiers1.Add(new Soldier(name: "Richard"));
-            _squadSoldiers1.Add(new Soldier(name: "Billy"));
-            _squadSoldiers1.Add(new Soldier(name: "Timothy"));
-            _squadSoldiers1.Add(new Soldier(name: "Bryan"));
-            _squadSoldiers1.Add(new Soldier(name: "Bob"));
-            _squadSoldiers1.Add(new Soldier(name: "Bobby"));
+            _squadSoldiers1.Add(new Soldier("Brandon"));
+            _squadSoldiers1.Add(new Soldier("Kevin"));
+            _squadSoldiers1.Add(new Soldier("Kenneth"));
+            _squadSoldiers1.Add(new Soldier("Bruce"));
+            _squadSoldiers1.Add(new Soldier("Billy"));
+            _squadSoldiers1.Add(new Soldier("Joseph"));
+            _squadSoldiers1.Add(new Soldier("Paul"));
+            _squadSoldiers1.Add(new Soldier("Richard"));
+            _squadSoldiers1.Add(new Soldier("Billy"));
+            _squadSoldiers1.Add(new Soldier("Timothy"));
+            _squadSoldiers1.Add(new Soldier("Bryan"));
+            _squadSoldiers1.Add(new Soldier("Bob"));
+            _squadSoldiers1.Add(new Soldier("Bobby"));
 
             _squadSoldiers2 = new List<Soldier>();
-            _squadSoldiers2.Add(new Soldier(name: "Benjamin"));
-            _squadSoldiers2.Add(new Soldier(name: "John"));
-            _squadSoldiers2.Add(new Soldier(name: "Kevin"));
-            _squadSoldiers2.Add(new Soldier(name: "Patrick"));
-            _squadSoldiers2.Add(new Soldier(name: "Nathan"));
-            _squadSoldiers2.Add(new Soldier(name: "Ronald"));
-            _squadSoldiers2.Add(new Soldier(name: "Bruce"));
-            _squadSoldiers2.Add(new Soldier(name: "Thomas"));
-            _squadSoldiers2.Add(new Soldier(name: "Eugene"));
-            _squadSoldiers2.Add(new Soldier(name: "Johnny"));
-            _squadSoldiers2.Add(new Soldier(name: "Philip"));
+            _squadSoldiers2.Add(new Soldier("Benjamin"));
+            _squadSoldiers2.Add(new Soldier("John"));
+            _squadSoldiers2.Add(new Soldier("Kevin"));
+            _squadSoldiers2.Add(new Soldier("Patrick"));
+            _squadSoldiers2.Add(new Soldier("Nathan"));
+            _squadSoldiers2.Add(new Soldier("Ronald"));
+            _squadSoldiers2.Add(new Soldier("Bruce"));
+            _squadSoldiers2.Add(new Soldier("Thomas"));
+            _squadSoldiers2.Add(new Soldier("Eugene"));
+            _squadSoldiers2.Add(new Soldier("Johnny"));
+            _squadSoldiers2.Add(new Soldier("Philip"));
         }
 
         public void TransferSoldier()
         {
-            string word = "B";
+            var word = "B";
             var squad = _squadSoldiers1.Where(soldier => soldier.Name.StartsWith(word));
             _squadSoldiers2 = squad.Union(_squadSoldiers2).ToList();
             _squadSoldiers1 = _squadSoldiers1.Except(squad).ToList();
@@ -60,28 +60,31 @@ namespace HomeWork_7_7
 
         public void Start()
         {
-            Console.WriteLine($"Squad Solisers1:");
+            Console.WriteLine("Squad Solisers1:");
             ShowArmy(_squadSoldiers1);
-            Console.WriteLine($"\nSquad Solisers2:");
+            Console.WriteLine("\nSquad Solisers2:");
             ShowArmy(_squadSoldiers2);
             Console.ReadLine();
         }
 
         private void ShowArmy(List<Soldier> soldiers)
         {
-            foreach (Soldier solider in soldiers)
-            {
-                solider.ShowInfo();
-            }
+            foreach (var solider in soldiers) solider.ShowInfo();
         }
     }
 
-    class Soldier
+    internal class Soldier
     {
-        public string Name { get; private set; }
+        public Soldier(string name)
+        {
+            Name = name;
+        }
 
-        public Soldier(string name) => Name = name;
+        public string Name { get; }
 
-        public void ShowInfo() => Console.WriteLine($"Name:{Name}");
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Name:{Name}");
+        }
     }
 }
